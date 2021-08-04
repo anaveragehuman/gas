c30de324d6ae5758015aaaf44a264d68cbd857c5
Jeffrey Lin <jeffrey@icurse.nl>
2021-08-04 15:50:47 -0400

    dashboard: add spinner to status indicator on load
    Inspired by Uniswap's Spinner style.

--- lib/widgets/status_indicator.dart
+++ lib/widgets/status_indicator.dart
@@ -15,6 +15,7 @@ class StatusIndicator extends StatelessWidget {
           return _StatusIndicator(
             status: state.info.lastBlock,
             color: colorGasState(state),
+            isLoading: state.isLoading,
           );
         },
       ),
@@ -25,11 +26,13 @@ class StatusIndicator extends StatelessWidget {
 class _StatusIndicator extends StatelessWidget {
   final String status;
   final Color color;
+  final bool isLoading;
 
   const _StatusIndicator({
     Key? key,
     required this.status,
     required this.color,
+    required this.isLoading,
   }) : super(key: key);
 
   @override
@@ -39,10 +42,81 @@ class _StatusIndicator extends StatelessWidget {
       children: [
         Text(status, style: TextStyle(color: color, fontSize: 16)),
         const SizedBox(width: 12),
-        Icon(Icons.circle, color: color, size: 16),
+        SizedBox(
+          width: 20,
+          child: Stack(
+            alignment: AlignmentDirectional.center,
+            children: [
+              Icon(Icons.circle, color: color, size: 16),
+              if (isLoading) Spinner(color: color, size: 20),
+            ],
+          ),
+        ),
       ],
     );
   }
+}
+
+class Spinner extends StatefulWidget {
+  final Color color;
+  final double size;
+
+  const Spinner({
+    Key? key,
+    required this.color,
+    required this.size,
+  }) : super(key: key);
+
+  @override
+  State<Spinner> createState() => _SpinnerState();
+}
+
+class _SpinnerState extends State<Spinner> with SingleTickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    vsync: this,
+    duration: const Duration(seconds: 1),
+  )..repeat();
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return RotationTransition(
+      turns: CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeInOutQuart,
+      ),
+      child: CustomPaint(
+        size: Size.square(widget.size),
+        painter: _ArcPainter(color: widget.color),
+      ),
+    );
+  }
+}
+
+class _ArcPainter extends CustomPainter {
+  final Color color;
+
+  const _ArcPainter({
+    required this.color,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = color
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = size.longestSide / 10;
+
+    canvas.drawArc(Offset.zero & size, 0, 2, false, paint);
+  }
+
+  @override
+  bool shouldRepaint(_ArcPainter oldDelegate) => oldDelegate.color != color;
 }
 
 Color colorGasState(GasState state) {
